d96ee0e82338961715b5238b8b7f918966f4846b
JuanRdBO <ctlndjuan@hotmail.com>
2023-07-24 08:04:52 -0600

    * feat: added my base64 repo

--- example/src/testing/Tests/RandomTests/random.ts
+++ example/src/testing/Tests/RandomTests/random.ts
@@ -30,59 +30,55 @@ import type { Done } from 'mocha';
 const assert = chai.assert;
 
 export function registerRandomTests() {
-  describe('check args', () => {
-    // TODO (Szymon)
-    [crypto.randomBytes, crypto.pseudoRandomBytes].forEach((f) => {
-      /*  [undefined, null, false, true, {}, []].forEach((value) => {
-        const errObj = {
-          code: 'ERR_INVALID_ARG_TYPE',
-          name: 'TypeError',
-          message:
-            'The "size" argument must be of type number.' +
-            common.invalidArgTypeHelper(value),
-        };
-        assert.throws(() => f(value), errObj);
-        assert.throws(() => f(value, common.mustNotCall()), errObj);
-      });
-
-      [-1, NaN, 2 ** 32, 2 ** 31].forEach((value) => {
-        const errObj = {
-          code: 'ERR_OUT_OF_RANGE',
-          name: 'RangeError',
-          message:
-            'The value of "size" is out of range. It must be >= 0 && <= ' +
-            `${kMaxPossibleLength}. Received ${value}`,
-        };
-        assert.throws(() => f(value), errObj);
-        assert.throws(() => f(value, common.mustNotCall()), errObj);
-      });*/
-
-      [0, 1, 2, 4, 16, 256, 1024, 101.2].forEach((len) => {
-        const length = len;
-        const funn = f;
-        it('function ' + funn + ' & len ' + length, (done: Done) => {
-          funn(length, (ex: any, buf: any) => {
-            try {
-              assert.strictEqual(ex, null);
-              assert.strictEqual(buf.length, Math.floor(len));
-              assert.ok(Buffer.isBuffer(buf));
-            } catch (e) {
-              done(e);
-            }
-            done();
-          });
-        });
-      });
-    });
-  });
-
+  // describe('check args', () => {
+  //   // TODO (Szymon)
+  //   [crypto.randomBytes, crypto.pseudoRandomBytes].forEach((f) => {
+  //     /*  [undefined, null, false, true, {}, []].forEach((value) => {
+  //       const errObj = {
+  //         code: 'ERR_INVALID_ARG_TYPE',
+  //         name: 'TypeError',
+  //         message:
+  //           'The "size" argument must be of type number.' +
+  //           common.invalidArgTypeHelper(value),
+  //       };
+  //       assert.throws(() => f(value), errObj);
+  //       assert.throws(() => f(value, common.mustNotCall()), errObj);
+  //     });
+  //     [-1, NaN, 2 ** 32, 2 ** 31].forEach((value) => {
+  //       const errObj = {
+  //         code: 'ERR_OUT_OF_RANGE',
+  //         name: 'RangeError',
+  //         message:
+  //           'The value of "size" is out of range. It must be >= 0 && <= ' +
+  //           `${kMaxPossibleLength}. Received ${value}`,
+  //       };
+  //       assert.throws(() => f(value), errObj);
+  //       assert.throws(() => f(value, common.mustNotCall()), errObj);
+  //     });*/
+  //     [0, 1, 2, 4, 16, 256, 1024, 101.2].forEach((len) => {
+  //       const length = len;
+  //       const funn = f;
+  //       it('function ' + funn + ' & len ' + length, (done: Done) => {
+  //         funn(length, (ex: any, buf: any) => {
+  //           try {
+  //             assert.strictEqual(ex, null);
+  //             assert.strictEqual(buf.length, Math.floor(len));
+  //             assert.ok(Buffer.isBuffer(buf));
+  //           } catch (e) {
+  //             done(e);
+  //           }
+  //           done();
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
   it('simple test (do sth)', () => {
     const buf = Buffer.alloc(10);
     const before = buf.toString('hex');
     const after = crypto.randomFillSync(buf).toString('hex');
     assert.notStrictEqual(before, after);
   });
-
   it('simple test (do sth) 2', () => {
     const buf = new Uint8Array(new Array(10).fill(0));
     const before = Buffer.from(buf).toString('hex');
@@ -90,7 +86,6 @@ export function registerRandomTests() {
     const after = Buffer.from(buf).toString('hex');
     assert.notStrictEqual(before, after);
   });
-
   it('simple test (do sth) 3', () => {
     [
       new Uint16Array(10),
@@ -105,7 +100,6 @@ export function registerRandomTests() {
       assert.notStrictEqual(before, after);
     });
   });
-
   it('simple test (do sth) 4 - random Fill Sync AB', () => {
     [new ArrayBuffer(10), new ArrayBuffer(10)].forEach((buf) => {
       const before = Buffer.from(buf).toString('hex');
@@ -114,11 +108,9 @@ export function registerRandomTests() {
       assert.notStrictEqual(before, after);
     });
   });
-
   it('simple test (do sth) 5- random Fill ', (done: Done) => {
     const buf = Buffer.alloc(10);
     const before = buf.toString('hex');
-
     crypto.randomFill(buf, (_, res) => {
       try {
         const after = res?.toString('hex');
@@ -129,11 +121,9 @@ export function registerRandomTests() {
       done();
     });
   });
-
   it('simple test (do sth) 6 ', (done: Done) => {
     const buf = new Uint8Array(new Array(10).fill(0));
     const before = Buffer.from(buf).toString('hex');
-
     crypto.randomFill(buf, (_, res) => {
       try {
         const after = Buffer.from(res).toString('hex');
@@ -144,7 +134,6 @@ export function registerRandomTests() {
       }
     });
   });
-
   it('simple test (do sth) 7', (done: Done) => {
     let ctr = 0;
     [
@@ -155,7 +144,6 @@ export function registerRandomTests() {
       new DataView(new ArrayBuffer(10)),
     ].forEach((buf) => {
       const before = Buffer.from(buf.buffer).toString('hex');
-
       crypto.randomFill(buf, (_err, buf) => {
         try {
           const after = Buffer.from(buf!.buffer).toString('hex');
@@ -170,7 +158,6 @@ export function registerRandomTests() {
       });
     });
   });
-
   it('simple test (do sth) 8', (done: Done) => {
     let ctr = 0;
     [new ArrayBuffer(10)].forEach((buf) => {
@@ -189,7 +176,6 @@ export function registerRandomTests() {
       });
     });
   });
-
   it('randomFillSync - deepStringEqual - Buffer', () => {
     const buf = Buffer.alloc(10);
     const before = buf.toString('hex');
@@ -198,7 +184,6 @@ export function registerRandomTests() {
     assert.notStrictEqual(before, after);
     assert.deepStrictEqual(before.slice(0, 5), after.slice(0, 5));
   });
-
   it('randomFillSync - deepStringEqual - Uint8Array', () => {
     const buf = new Uint8Array(new Array(10).fill(0));
     const before = Buffer.from(buf).toString('hex');
@@ -207,7 +192,6 @@ export function registerRandomTests() {
     assert.notStrictEqual(before, after);
     assert.deepStrictEqual(before.slice(0, 5), after.slice(0, 5));
   });
-
   it('randomFillSync - deepStringEqual - Buffer no size', () => {
     const buf = Buffer.alloc(10);
     const before = buf.toString('hex');
@@ -216,11 +200,9 @@ export function registerRandomTests() {
     assert.notStrictEqual(before, after);
     assert.deepStrictEqual(before.slice(0, 5), after.slice(0, 5));
   });
-
   it('randomFill - deepStringEqual - Buffer', (done: Done) => {
     const buf = Buffer.alloc(10);
     const before = buf.toString('hex');
-
     crypto.randomFill(buf, 5, 5, (_err, res) => {
       try {
         const after = res.toString('hex');
@@ -232,7 +214,6 @@ export function registerRandomTests() {
       done();
     });
   });
-
   it('randomFill - deepStringEqual - Uint8Array', (done: Done) => {
     const buf = new Uint8Array(new Array(10).fill(0));
     const before = Buffer.from(buf).toString('hex');
@@ -247,7 +228,6 @@ export function registerRandomTests() {
       done();
     });
   });
-
   //   finish
   /*describe('errors checks', () => {
     [Buffer.alloc(10), new Uint8Array(new Array(10).fill(0))].forEach((buf) => {
@@ -256,7 +236,6 @@ export function registerRandomTests() {
         const len = Buffer.byteLength(buffer);
         assert.strictEqual(len, 10, `Expected byteLength of 10, got ${len}`);
       });
-
       const typeErrObj = {
         code: 'ERR_INVALID_ARG_TYPE',
         name: 'TypeError',
@@ -264,7 +243,6 @@ export function registerRandomTests() {
           'The "offset" argument must be of type number. ' +
           "Received type string ('test')",
       };
-
       it('offset must be a number', () => {
         assert.throws(
           () => crypto.randomFillSync(buffer, 'test'),
@@ -272,22 +250,18 @@ export function registerRandomTests() {
           typeErrObj.message
         );
       });
-
       it('offsetMustBe a number ', () => {
         assert.throws(
           () => crypto.randomFill(buffer, 'test', () => {}),
           typeErrObj
         );
       });
-
       typeErrObj.message = typeErrObj.message.replace('offset', 'size');
       assert.throws(() => crypto.randomFillSync(buffer, 0, 'test'), typeErrObj);
-
       assert.throws(
         () => crypto.randomFill(buffer, 0, 'test', () => {})),
         typeErrObj
       );
-
       [NaN, kMaxPossibleLength + 1, -10, (-1 >>> 0) + 1].forEach(
         (offsetSize) => {
           const errObj = {
@@ -297,14 +271,11 @@ export function registerRandomTests() {
               'The value of "offset" is out of range. ' +
               `It must be >= 0 && <= 10. Received ${offsetSize}`,
           };
-
           assert.throws(() => crypto.randomFillSync(buf, offsetSize), errObj);
-
           assert.throws(
             () => crypto.randomFill(buffer, offsetSize, () => {}),
             errObj
           );
-
           errObj.message =
             'The value of "size" is out of range. It must be >= ' +
             `0 && <= ${kMaxPossibleLength}. Received ${offsetSize}`;
@@ -312,14 +283,12 @@ export function registerRandomTests() {
             () => crypto.randomFillSync(buffer, 1, offsetSize),
             errObj
           );
-
           assert.throws(
             () => crypto.randomFill(buffer, 1, offsetSize, () => {}),
             errObj
           );
         }
       );
-
       const rangeErrObj = {
         code: 'ERR_OUT_OF_RANGE',
         name: 'RangeError',
@@ -328,11 +297,9 @@ export function registerRandomTests() {
           'It must be <= 10. Received 11',
       };
       assert.throws(() => crypto.randomFillSync(buf, 1, 10), rangeErrObj);
-
       assert.throws(() => crypto.randomFill(buf, 1, 10, () => {}), rangeErrObj);
     });
   });*/
-
   // https://github.com/nodejs/node-v0.x-archive/issues/5126,
   // "FATAL ERROR: v8::Object::SetIndexedPropertiesToExternalArrayData() length
   // exceeds max acceptable value"
@@ -344,7 +311,6 @@ export function registerRandomTests() {
       'The value of "size" is out of range. ' +
       `It must be >= 0 && <= ${kMaxPossibleLength}. Received 4294967296`,
   });
-
   [1, true, NaN, null, undefined, {}, []].forEach((i) => {
     const buf = Buffer.alloc(10);
     assert.throws(() => crypto.randomFillSync(i), {
@@ -360,14 +326,12 @@ export function registerRandomTests() {
       name: 'TypeError',
     });
   });
-
   [1, true, NaN, null, {}, []].forEach((i) => {
     assert.throws(() => crypto.randomBytes(1, i), {
       code: 'ERR_INVALID_ARG_TYPE',
       name: 'TypeError',
     });
   }); */
-
   ['pseudoRandomBytes', 'prng', 'rng'].forEach((f) => {
     const name = f;
     it(name, () => {
@@ -377,7 +341,6 @@ export function registerRandomTests() {
       assert.strictEqual(desc?.enumerable, false);
     });
   });
-
   it('randomInt - Asyncynchronous API', (done: Done) => {
     const randomInts: number[] = [];
     let failed = false;
@@ -404,7 +367,6 @@ export function registerRandomTests() {
       });
     }
   });
-
   it('randomInt - Synchronous API', () => {
     const randomInts = [];
     for (let i = 0; i < 100; i++) {
@@ -413,14 +375,12 @@ export function registerRandomTests() {
       assert.ok(n < 3);
       randomInts.push(n);
     }
-
     assert.ok(!randomInts.includes(-1));
     assert.ok(randomInts.includes(0));
     assert.ok(randomInts.includes(1));
     assert.ok(randomInts.includes(2));
     assert.ok(!randomInts.includes(3));
   });
-
   it('randomInt positive range', (done: Done) => {
     const randomInts: number[] = [];
     let failed = false;
@@ -444,7 +404,6 @@ export function registerRandomTests() {
       });
     }
   });
-
   it('randomInt negative range', (done: Done) => {
     const randomInts: number[] = [];
     let failed = false;
@@ -486,7 +445,6 @@ export function registerRandomTests() {
           'The "max" argument must be a safe integer.' +
           `${common.invalidArgTypeHelper(i)}`,
       };
-
       assert.throws(() => crypto.randomInt(i, 100), invalidMinError);
       assert.throws(
         () => crypto.randomInt(i, 100, common.mustNotCall()),
@@ -503,7 +461,6 @@ export function registerRandomTests() {
       );
       assert.throws(() => crypto.randomInt(0, i), invalidMaxError);
     });
-
     assert.throws(
       () => crypto.randomInt(minInt - 1, minInt + 5, common.mustNotCall()),
       {
@@ -514,7 +471,6 @@ export function registerRandomTests() {
           `${common.invalidArgTypeHelper(minInt - 1)}`,
       }
     );
-
     assert.throws(() => crypto.randomInt(maxInt + 1, common.mustNotCall()), {
       code: 'ERR_INVALID_ARG_TYPE',
       name: 'TypeError',
@@ -522,7 +478,6 @@ export function registerRandomTests() {
         'The "max" argument must be a safe integer.' +
         `${common.invalidArgTypeHelper(maxInt + 1)}`,
     });*/
-
     describe('ERR_OUT_OF_RANGE simple', () => {
       for (const arg of [[0], [1, 1], [3, 2], [-5, -5], [11, -10]]) {
         const interval = arg;
@@ -537,49 +492,41 @@ export function registerRandomTests() {
         });
       }
     });
-
     const MAX_RANGE = 0xffffffffffff;
     describe('must succeed', () => {
       const maxInt = Number.MAX_SAFE_INTEGER;
       const minInt = Number.MIN_SAFE_INTEGER;
-
       it('minInt, minInt + 5 ', (done: Done) => {
         crypto.randomInt(minInt, minInt + 5, () => {
           done();
         });
       });
-
       it('maxint - 5, maxint', (done: Done) => {
         crypto.randomInt(maxInt - 5, maxInt, () => {
           done();
         });
       });
-
       it('1', (done: Done) => {
         crypto.randomInt(1, () => {
           done();
         });
       });
-
       it('0 - 1', (done: Done) => {
         crypto.randomInt(0, 1, () => {
           done();
         });
       });
-
       it('maxRange', (done: Done) => {
         crypto.randomInt(MAX_RANGE, () => {
           done();
         });
       });
-
       it('maxRange move + 1', (done: Done) => {
         crypto.randomInt(1, MAX_RANGE + 1, () => {
           done();
         });
       });
     });
-
     it('ERR_OUT_OF_RANGE 1', () => {
       assert.throws(
         () => crypto.randomInt(1, MAX_RANGE + 2, () => {}),
@@ -589,7 +536,6 @@ export function registerRandomTests() {
           'Received 281_474_976_710_656'
       );
     });
-
     it('ERR_OUT_OF_RANGE 2', () => {
       assert.throws(
         () => crypto.randomInt(MAX_RANGE + 1, () => {}),
@@ -599,7 +545,6 @@ export function registerRandomTests() {
           'Received 281_474_976_710_656'
       );
     });
-
     [true, NaN, null, {}, [], 10].forEach((i) => {
       const val = i;
       it('expect type error', () => {
@@ -611,7 +556,6 @@ export function registerRandomTests() {
       });
     });
   }
-
   describe("Verify that it doesn't throw or abort", () => {
     it('int16', (done: Done) => {
       crypto.randomFill(new Uint16Array(10), 0, () => {
